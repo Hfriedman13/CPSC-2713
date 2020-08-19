af324f7dc8983e8c96835daf0615de0ec6d19cf4
Hannah Friedman <hzf0023@auburn.edu>
2020-08-19 14:35:12 -0700

    Declared variables for FirstConsoleApplication and created simple application that prompts users for two numbers and an operation. Outputs results to console.

--- FirstSolution/FirstConsoleApplication/Program.cs
+++ FirstSolution/FirstConsoleApplication/Program.cs
@@ -6,7 +6,38 @@ namespace FirstConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1, num2, result; //declaring variables
+
+            Console.WriteLine("Please enter first number: "); //1st num
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter a second number: "); //2nd num
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please choose to 1)Add, 2)subtract, or 3)multiply the two numbers: "); //operation option
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            if(option == 1) //addition
+            {
+                result = num1 + num2;
+                Console.WriteLine("The result of " + num1.ToString() + " + " + num2.ToString() + " = " + result.ToString());
+            }
+            else if (option == 2) //subtraction
+            {
+                result = num1 - num2;
+                Console.WriteLine("The result of " + num1.ToString() + " - " + num2.ToString() + " = " + result.ToString());
+            }
+            else if (option == 3) //multiplication
+            {
+                result = num1 * num2;
+                Console.WriteLine("The result of " + num1.ToString() + " * " + num2.ToString() + " = " + result.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice"); //did not choose 1,2, or 3 
+            }
+            
+
         }
     }
 }
