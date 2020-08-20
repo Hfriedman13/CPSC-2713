d84fd9a85afd0cd1d906023482211e68ea582f7f
Hannah Friedman <hzf0023@auburn.edu>
2020-08-20 09:41:36 -0700

    added labels for txtboxes and completed Form1

--- FirstSolution/FirstFormsApplication/Form1.Designer.cs
+++ FirstSolution/FirstFormsApplication/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace FirstFormsApplication
+﻿using System;
+
+namespace FirstFormsApplication
 {
     partial class Form1
     {
@@ -33,13 +35,18 @@
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.tbNum2 = new System.Windows.Forms.TextBox();
             this.tbOption = new System.Windows.Forms.TextBox();
-            this.btnResults = new System.Windows.Forms.Button();
-            this.lblResults = new System.Windows.Forms.Label();
+            this.CalculateBtn = new System.Windows.Forms.Button();
+            this.resultsLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tbNum1
             // 
-            this.tbNum1.Location = new System.Drawing.Point(121, 93);
+            this.tbNum1.Location = new System.Drawing.Point(361, 43);
             this.tbNum1.Name = "tbNum1";
             this.tbNum1.Size = new System.Drawing.Size(100, 31);
             this.tbNum1.TabIndex = 0;
@@ -53,43 +60,95 @@
             // 
             // tbNum2
             // 
-            this.tbNum2.Location = new System.Drawing.Point(249, 93);
+            this.tbNum2.Location = new System.Drawing.Point(361, 120);
             this.tbNum2.Name = "tbNum2";
             this.tbNum2.Size = new System.Drawing.Size(100, 31);
             this.tbNum2.TabIndex = 2;
             // 
             // tbOption
             // 
-            this.tbOption.Location = new System.Drawing.Point(364, 93);
+            this.tbOption.Location = new System.Drawing.Point(361, 197);
             this.tbOption.Name = "tbOption";
             this.tbOption.Size = new System.Drawing.Size(100, 31);
             this.tbOption.TabIndex = 3;
             // 
-            // btnResults
+            // CalculateBtn
             // 
-            this.btnResults.Location = new System.Drawing.Point(507, 204);
-            this.btnResults.Name = "btnResults";
-            this.btnResults.Size = new System.Drawing.Size(118, 40);
-            this.btnResults.TabIndex = 4;
-            this.btnResults.Text = "Do Math!";
-            this.btnResults.UseVisualStyleBackColor = true;
+            this.CalculateBtn.Location = new System.Drawing.Point(361, 284);
+            this.CalculateBtn.Name = "CalculateBtn";
+            this.CalculateBtn.Size = new System.Drawing.Size(118, 40);
+            this.CalculateBtn.TabIndex = 4;
+            this.CalculateBtn.Text = "Do Math!";
+            this.CalculateBtn.UseVisualStyleBackColor = true;
+            this.CalculateBtn.Click += new System.EventHandler(this.CalculateBtn_Click);
             // 
-            // lblResults
+            // resultsLabel
             // 
-            this.lblResults.AutoSize = true;
-            this.lblResults.Location = new System.Drawing.Point(526, 286);
-            this.lblResults.Name = "lblResults";
-            this.lblResults.Size = new System.Drawing.Size(70, 25);
-            this.lblResults.TabIndex = 5;
-            this.lblResults.Text = "label1";
+            this.resultsLabel.AutoSize = true;
+            this.resultsLabel.Location = new System.Drawing.Point(250, 363);
+            this.resultsLabel.Name = "resultsLabel";
+            this.resultsLabel.Size = new System.Drawing.Size(96, 25);
+            this.resultsLabel.TabIndex = 5;
+            this.resultsLabel.Text = "Results: ";
+            this.resultsLabel.Click += new System.EventHandler(this.resultsLabel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(158, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "First Number: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(158, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Second Number: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(168, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Operation: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(107, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(216, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "1) \" + \"  2) \" - \"  3) \" * \"";
+            this.label5.Click += new System.EventHandler(this.resultsLabel_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblResults);
-            this.Controls.Add(this.btnResults);
+            this.ClientSize = new System.Drawing.Size(698, 562);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultsLabel);
+            this.Controls.Add(this.CalculateBtn);
             this.Controls.Add(this.tbOption);
             this.Controls.Add(this.tbNum2);
             this.Controls.Add(this.tbNum1);
@@ -101,14 +160,34 @@
 
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void resultsLabel_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
         #endregion
 
         private System.Windows.Forms.TextBox tbNum1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.TextBox tbNum2;
         private System.Windows.Forms.TextBox tbOption;
-        private System.Windows.Forms.Button btnResults;
-        private System.Windows.Forms.Label lblResults;
+        private System.Windows.Forms.Button CalculateBtn;
+        private System.Windows.Forms.Label resultsLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- FirstSolution/FirstFormsApplication/Form1.cs
+++ FirstSolution/FirstFormsApplication/Form1.cs
@@ -16,15 +16,31 @@ namespace FirstFormsApplication
         {
             InitializeComponent();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
+        private void CalculateBtn_Click(object sender, EventArgs e)
         {
+            int num1, num2, result;                    //declaring variables
+            num1 = Convert.ToInt32(tbNum1.Text);
+            num2 = Convert.ToInt32(tbNum2.Text);
+            String option = Convert.ToString(tbOption.Text);
 
-        }
+            if (option.Equals("+")) //additon
+            {
+                result = num1 + num2;
+                resultsLabel.Text = result.ToString();
+            }
+            else if (option.Equals("-")) //sub
+            {
+                result = num1 - num2;
+                resultsLabel.Text = result.ToString();
+            }
+            else if (option.Equals("*"))//multi 
+            {
+                result = num1 * num2;
+                resultsLabel.Text = result.ToString();
+            }
+            else
+                resultsLabel.Text = "Invalid choice"; //chose invaild choice
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
+        }   
     }
 }
